178eea06d4b33bdccac2baec7e4ef2c205f04c52
MueRTe <mu3r73@yandex.com>
2026-02-24 09:48:52 -0300

    decrapified

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { TestBed } from '@angular/core/testing'
+import { App } from './app'
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-    }).compileComponents();
-  });
+    }).compileComponents()
+  })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-signals-n-change-detection');
-  });
-});
+    const fixture = TestBed.createComponent(App)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+})
